bdc05bd498a3209d5d3661293849b2f9ace82df9
TG8211 <jwad1425@gmail.com>
2023-09-20 21:26:11 +0300

    c

--- assets/js/hoverChangeDescription.js
+++ assets/js/hoverChangeDescription.js
@@ -6,6 +6,6 @@ export function hoverChangeDescription(nameCard, text) {
   });
 
   document.querySelector(nameCard).addEventListener("mouseout", () => {
-    changeDescription.innerHTML = `*passe o cursor do mouse no card para ler*`;
+    changeDescription.innerHTML = `*Hover your mouse cursor over the card to read*`;
   });
 }
