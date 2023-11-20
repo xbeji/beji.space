1f31059157a950436b4b501d6b177a49ba2a4026
TG8211 <jwad1425@gmail.com>
2023-11-21 01:24:11 +0300

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,38 @@ window.addEventListener('contextmenu', function (e) {
 window.addEventListener('keydown', function (e) {
   e.preventDefault();
 });
+
+// Add this to your existing main.js file or create a new one
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Set the date for graduation (June 2026)
+  const graduationDate = new Date("June 1, 2026 00:00:00").getTime();
+
+  // Update the countdown every second
+  const countdownElement = document.getElementById("countdown");
+
+  function updateCountdown() {
+    const currentDate = new Date().getTime();
+    const timeRemaining = graduationDate - currentDate;
+
+    if (timeRemaining > 0) {
+      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor(
+        (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+      countdownElement.innerHTML = `
+        Graduation: ${days}d ${hours}h ${minutes}m ${seconds}s
+      `;
+    } else {
+      countdownElement.innerHTML = "Graduation has arrived!";
+    }
+  }
+
+  setInterval(updateCountdown, 1000);
+  updateCountdown(); // Initial call to set the initial value
+});
