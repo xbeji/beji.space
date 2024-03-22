491a0737fbdcdadccc51ccc12c568476322f2276
TG8211 <jwad1425@gmail.com>
2024-03-23 00:33:26 +0300

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,17 +172,33 @@ window.onload = function() {
 
 
 
-$(window).mousemove(function(e) {
-  $(".cursor").css({
-    left: e.pageX,
-    top: e.pageY
-  });
-});
+(function () {
 
-$("a").on("mouseenter", function() {
-  $('.cursor').addClass("active");
-});
+  const link = document.querySelectorAll('nav > .hover-this');
+  const cursor = document.querySelector('.cursor');
 
-$("a").on("mouseleave", function() {
-  $('.cursor').removeClass("active");
-});
+  const animateit = function (e) {
+        const span = this.querySelector('span');
+        const { offsetX: x, offsetY: y } = e,
+        { offsetWidth: width, offsetHeight: height } = this,
+
+        move = 25,
+        xMove = x / width * (move * 2) - move,
+        yMove = y / height * (move * 2) - move;
+
+        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+        if (e.type === 'mouseleave') span.style.transform = '';
+  };
+
+  const editCursor = e => {
+        const { clientX: x, clientY: y } = e;
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px';
+  };
+
+  link.forEach(b => b.addEventListener('mousemove', animateit));
+  link.forEach(b => b.addEventListener('mouseleave', animateit));
+  window.addEventListener('mousemove', editCursor);
+
+})();
